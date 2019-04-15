64cb321a3e0402851ebdfe8e28cecd7af368beeb
NeuroWhAI <neurowhai@gmail.com>
2019-04-15 15:15:52 +0900

    Do not print db url

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -21,7 +21,6 @@ thread_local! {
 fn establish_connection() -> PgConnection {
     let database_url = env::var("DATABASE_URL")
         .expect("DATABASE_URL must be set");
-    println!("{}", &database_url);
     PgConnection::establish(&database_url)
         .expect(&format!("Error connecting to {}", database_url))
 }
